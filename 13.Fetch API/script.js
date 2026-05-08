f4784fb4f04d81fd0a1a976d9ee90533109fbb7f
Hamza Zahid <hamzazahid0066@gmail.com>
2026-05-08 13:04:37 +0500

    Fetching API through Async Function

--- 13.Fetch API/script.js
+++ 13.Fetch API/script.js
@@ -1,9 +1,15 @@
 let url = "https://official-joke-api.appspot.com/random_joke";
 
-fetch(url)
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        console.log(data);
-    });
+// fetch(url)
+//     .then((response)=>{
+//         return response.json();
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//     });
+
+async function getJoke(){
+    const response = await fetch(url);
+    console.log(response);
+}
+getJoke();
